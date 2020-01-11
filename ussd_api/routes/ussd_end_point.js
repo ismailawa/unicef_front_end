7849da26d910de279e055bc3d45cb6129a40e0ac
ISMAILAWA <ismailawa.aliyu@gmail.com>
2020-01-11 14:24:50 +0100

    initial push heroku 3

--- ussd_api/routes/ussd_end_point.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const controllers = require('../controllers/ussd_controllers');
-
-router.route('/').post(controllers.questionaireController);
-
-module.exports = router;
