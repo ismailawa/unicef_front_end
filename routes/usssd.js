f22653e4e921000214972cf1ca9359a703531349
ISMAILAWA <ismailawa.aliyu@gmail.com>
2020-01-11 15:06:01 +0100

    add controllerlive test

--- routes/usssd.js
+++ routes/usssd.js
@@ -3,6 +3,6 @@ var router = express.Router();
 var controllers = require('../ussd_app/ussd_end_point')
 
 
-router.route('/').get(controllers.resgisterQuestionaire);
+router.route('/').post(controllers.resgisterQuestionaire);
 
 module.exports = router;
--- ussd_app/ussd_end_point.js
+++ ussd_app/ussd_end_point.js
@@ -1,3 +1,18 @@
+const UssdMenu = require('ussd-menu-builder');
+var menu = new UssdMenu();
+
 exports.resgisterQuestionaire = (req,res,next)=>{
-    res.send({"ismailawa":"you rock"});
+    menu.run(req.body, ussdResult => {
+        res.send(ussdResult);
+    });
 };
+
+
+menu.startState({
+    run: ()=> {
+        menu.con('Welcome. Choose option:' +
+        '\n1. trying' +
+        '\n2. catching');
+    },
+    defaultNext : "getstate"
+})
