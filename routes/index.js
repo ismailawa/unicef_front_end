7268f0c9a69569f41db8f2c0182abcd0c6c0476d
ISMAILAWA <ismailawa.aliyu@gmail.com>
2020-01-12 12:48:40 +0100

    not working 2

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
 const State = require('../ussd_app/models/state')
+const LGA = require('../ussd_app/models/local_area');
 
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+
     State.find()
     .exec()
     .then((state)=>{
