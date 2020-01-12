726ea41f2189923692d5d727d04def45ab1111a5
ISMAILAWA <ismailawa.aliyu@gmail.com>
2020-01-12 11:47:41 +0100

    not working 1

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ussdRouter = require('./routes/usssd');
 
-const uri = "mongodb+srv://unicef_user:password654321@cluster0-8id7m.mongodb.net/unicefdatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://unicef_user:password654321@cluster0-8id7m.mongodb.net/unicefdatabase?retryWrites=true";
 
 mongoose.connect(uri, {useNewUrlParser: true });
 
