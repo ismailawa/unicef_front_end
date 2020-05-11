5c8bfbbce7528268717b5f1533f2a30ebbde3650
ISMAILAWA <ismailawa.aliyu@gmail.com>
2020-05-11 14:31:33 +0100

    corrected layout issue

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ let app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.engine('ejs',engine.__express);
 
 app.use(logger('dev'));
 app.use(express.json());
